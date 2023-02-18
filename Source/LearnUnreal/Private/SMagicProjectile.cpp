21ae539f117fae8956e3b8ea379c6ca030b6040b
Haoran Dong <haorandm@gmail.com>
2023-02-17 22:23:54 -0500

    - started on AI

--- Source/LearnUnreal/Private/SMagicProjectile.cpp
+++ Source/LearnUnreal/Private/SMagicProjectile.cpp
@@ -1,58 +1,16 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #include "SMagicProjectile.h"
-#include "Components/SphereComponent.h"
-#include "GameFramework/ProjectileMovementComponent.h"
-#include "Particles/ParticleSystemComponent.h"
 #include "SAttributeComponent.h"
 
-// Sets default values
-ASMagicProjectile::ASMagicProjectile()
-{
-    // Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
-    PrimaryActorTick.bCanEverTick = true;
-
-    SphereComp = CreateDefaultSubobject<USphereComponent>("SphereComp");
-
-    // Could setup collision manually as well.
-    // SphereComp->SetCollisionObjectType(ECC_WorldDynamic);
-    // SphereComp->SetCollisionResponseToAllChannels(ECR_Ignore);
-    // SphereComp->SetCollisionResponseToChannel(ECC_Pawn, ECR_Overlap);
-    // This profile was created in project settings
-    SphereComp->SetCollisionProfileName("Projectile");
-    SphereComp->OnComponentBeginOverlap.AddDynamic(this, &ASMagicProjectile::OnActorOverlap); // TODO: Maybe switch on hit as suggested by documentation
-    RootComponent = SphereComp;
-
-    EffectComp = CreateDefaultSubobject<UParticleSystemComponent>("EffectComp");
-    EffectComp->SetupAttachment(SphereComp);
-
-    MovementComp = CreateAbstractDefaultSubobject<UProjectileMovementComponent>("MovementComp");
-    MovementComp->InitialSpeed = 4000.f;
-    MovementComp->bRotationFollowsVelocity = true;
-    MovementComp->bInitialVelocityInLocalSpace = true;
-}
-
-// Called when the game starts or when spawned
-void ASMagicProjectile::BeginPlay()
-{
-    Super::BeginPlay();
-}
-
-void ASMagicProjectile::OnActorOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor,
-    UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep,
-    const FHitResult& SweepResult)
+void ASMagicProjectile::OnActorHit(UPrimitiveComponent* HitComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, FVector NormalImpulse, const FHitResult& Hit)
 {
     if (OtherActor) {
         USAttributeComponent* AttributeComp = Cast<USAttributeComponent>(OtherActor->GetComponentByClass(USAttributeComponent::StaticClass()));
         if (AttributeComp) {
             AttributeComp->ApplyHealthChange(-20.f);
-            Destroy();
         }
     }
-}
-
-// Called every frame
-void ASMagicProjectile::Tick(float DeltaTime)
-{
-    Super::Tick(DeltaTime);
+    // TODO: Do I put this in else?
+    ASProjectileBase::OnActorHit(HitComponent, OtherActor, OtherComp, NormalImpulse, Hit);
 }
--- Source/LearnUnreal/Public/SMagicProjectile.h
+++ Source/LearnUnreal/Public/SMagicProjectile.h
@@ -1,46 +1,21 @@
-// Fill out your copyright notice in the Description page of Project Settings.
+// Fill out your copyriht notice in the Description page of Project Settings.
 
 #pragma once
 
 #include "CoreMinimal.h"
 #include "GameFramework/Actor.h"
+#include "SProjectileBase.h"
 #include "SMagicProjectile.generated.h"
 
-class USphereComponent;
-class UProjectileMovementComponent;
-class UParticleSystemComponent;
-
 UCLASS()
-class LEARNUNREAL_API ASMagicProjectile : public AActor {
+class LEARNUNREAL_API ASMagicProjectile : public ASProjectileBase {
     GENERATED_BODY()
 
-public:
-    // Sets default values for this actor's properties
-    ASMagicProjectile();
-
 protected:
-    // Called when the game starts or when spawned
-    virtual void BeginPlay() override;
-
-    UPROPERTY(VisibleAnywhere, BlueprintReadonly)
-    USphereComponent* SphereComp;
-
-    UPROPERTY(VisibleAnywhere, BlueprintReadonly)
-    UProjectileMovementComponent* MovementComp;
-
-    UPROPERTY(VisibleAnywhere, BlueprintReadonly)
-    UParticleSystemComponent* EffectComp;
-
-    UFUNCTION()
-    void OnActorOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor,
-        UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep,
-        const FHitResult& SweepResult);
-
-public:
-    // Called every frame
-    virtual void Tick(float DeltaTime) override;
+    virtual void OnActorHit(UPrimitiveComponent* HitComponent, AActor* OtherActor,
+        UPrimitiveComponent* OtherComp, FVector NormalImpulse,
+        const FHitResult& Hit) override;
 };
-
 // TODO: Maybe ditch the hit event and go for the overlap event and then decide what to do with the projectile.
 // i.e. go through if the other actor is owner/teammate and explode/deal damage and destroy self in other cases.
 
